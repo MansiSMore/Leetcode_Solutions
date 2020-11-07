9e6c3c25b7fef35e711bbacebf79f087ee049a0b
Mansi More <45144625+MansiSMore@users.noreply.github.com>
2020-11-07 19:53:47 +0530

    Create 141_Linked_List_Cycle.c

---
+++ 141_Linked_List_Cycle.c
@@ -0,0 +1,43 @@
+/*
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+Example 1:
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+Example 2:
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 0th node.
+
+Example 3:
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool hasCycle(struct ListNode *head) 
+{
+    struct ListNode *t1 = head, *t2 = head;
+    
+    while(t2 != NULL && t2-> next != NULL)
+    {
+        t1 = t1 -> next;
+        t2 = t2 -> next -> next;
+        if(t1 == t2)
+            return true;
+    }
+    return false;
+}
