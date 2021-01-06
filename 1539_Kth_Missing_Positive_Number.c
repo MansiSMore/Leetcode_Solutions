b83f786a81551daeaebb0abba77906609efbdf6d
Mansi More <45144625+MansiSMore@users.noreply.github.com>
2021-01-06 21:04:23 +0530

    Create 1539_Kth_Missing_Positive_Number.c

---
+++ 1539_Kth_Missing_Positive_Number.c
@@ -0,0 +1,50 @@
+/*
+Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+Find the kth positive integer that is missing from this array.
+
+Example 1:
+Input: arr = [2,3,4,7,11], k = 5
+Output: 9
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+
+Example 2:
+Input: arr = [1,2,3,4], k = 2
+Output: 6
+Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6.
+
+Constraints:
+1 <= arr.length <= 1000
+1 <= arr[i] <= 1000
+1 <= k <= 1000
+arr[i] < arr[j] for 1 <= i < j <= arr.length
+
+*/
+int findKthPositive(int* arr, int arrSize, int k)
+{
+    int i = 0, no = 1, cnt = 1;
+    
+    while(i < arrSize) 
+    {
+        if(no != arr[i])
+        {
+            if(cnt == k)
+            {
+                //printf("%d\n",no);
+                return no;
+            }
+            //printf("%d\n",no);
+            cnt++;
+        }
+        else
+            i++;
+        no++;
+    }
+    
+    while(k != cnt)
+    {
+        no++;
+        cnt++;
+    }
+    return no;
+
+}
