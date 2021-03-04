accc84e0e7f7ea64bc63c7347fcccc70031af6c7
Mansi More <45144625+MansiSMore@users.noreply.github.com>
2021-03-04 22:59:44 +0530

    Create 1305_All_Elements_in_Two_Binary_Search_Trees.cpp

---
+++ 1305_All_Elements_in_Two_Binary_Search_Trees.cpp
@@ -0,0 +1,65 @@
+/*
+Given two binary search trees root1 and root2.
+Return a list containing all the integers from both trees sorted in ascending order.
+
+Example 1:
+Input: root1 = [2,1,4], root2 = [1,0,3]
+Output: [0,1,1,2,3,4]
+
+Example 2:
+Input: root1 = [0,-10,10], root2 = [5,1,7,0,2]
+Output: [-10,0,0,1,2,5,7,10]
+
+Example 3:
+Input: root1 = [], root2 = [5,1,7,0,2]
+Output: [0,1,2,5,7]
+
+Example 4:
+Input: root1 = [0,-10,10], root2 = []
+Output: [-10,0,10]
+
+Example 5:
+Input: root1 = [1,null,8], root2 = [8,1]
+Output: [1,1,8,8]
+
+Constraints:
+Each tree has at most 5000 nodes.
+Each node's value is between [-10^5, 10^5].
+
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+class Solution 
+{
+public:
+    vector<int> ans;
+    
+    void inorder(TreeNode* root)
+    {
+        if(root == NULL)
+            return;
+            inorder(root -> left);
+            ans.push_back(root -> val);
+            inorder(root -> right);
+        
+    }
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) 
+    {
+        inorder(root1);
+        inorder(root2);
+        
+        sort(ans.begin(), ans.end());        
+        return ans;
+    }
+};
