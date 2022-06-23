661e3ced95f53a4ea175813229329d431fdbcc32
Mansi More <45144625+MansiSMore@users.noreply.github.com>
2022-06-23 19:11:00 +0530

    Create 2000_Reverse_Prefix_of_Word.cpp

---
+++ 2000_Reverse_Prefix_of_Word.cpp
@@ -0,0 +1,62 @@
+/*
+Given a 0-indexed string word and a character ch, 
+reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). 
+If the character ch does not exist in word, do nothing.
+For example, if word = "abcdefd" and ch = "d", then you should reverse the segment that starts at 0 and ends at 3 (inclusive). 
+The resulting string will be "dcbaefd".
+Return the resulting string.
+
+Example 1:
+Input: word = "abcdefd", ch = "d"
+Output: "dcbaefd"
+Explanation: The first occurrence of "d" is at index 3. 
+Reverse the part of word from 0 to 3 (inclusive), the resulting string is "dcbaefd".
+
+Example 2:
+Input: word = "xyxzxe", ch = "z"
+Output: "zxyxxe"
+Explanation: The first and only occurrence of "z" is at index 3.
+Reverse the part of word from 0 to 3 (inclusive), the resulting string is "zxyxxe".
+
+Example 3:
+Input: word = "abcd", ch = "z"
+Output: "abcd"
+Explanation: "z" does not exist in word.
+You should not do any reverse operation, the resulting string is "abcd".
+
+Constraints:
+1 <= word.length <= 250
+word consists of lowercase English letters.
+ch is a lowercase English letter.
+
+*/
+class Solution {
+public:
+    string reversePrefix(string word, char ch) 
+    {
+        int pos = 0;
+        
+        for(int i = 0; i < word.length(); i++)
+        {
+            pos++;
+            if(word[i] == ch)
+            {
+                break;
+            }
+        }
+        
+        if(pos == word.length() && word[word.length() - 1] != ch)
+            return word;
+        
+        int i = 0, j = pos - 1;
+        while(i < j)
+        {
+            char ch = word[i];
+            word[i] = word[j];
+            word[j] = ch;
+            i++, j--;
+        }
+        //cout << word << endl;
+        return word;
+    }
+};
