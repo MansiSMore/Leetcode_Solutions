43bbb35b4628a165ad955ad787676cf4cbe1cf7c
Mansi More <45144625+MansiSMore@users.noreply.github.com>
2022-06-30 21:05:27 +0530

    Create 1991_Find_the_Middle_Index_in_Array.cpp

---
+++ 1991_Find_the_Middle_Index_in_Array.cpp
@@ -0,0 +1,56 @@
+/*
+Given a 0-indexed integer array nums, find the leftmost middleIndex (i.e., the smallest amongst all the possible ones).
+A middleIndex is an index where nums[0] + nums[1] + ... + nums[middleIndex-1] == nums[middleIndex+1] + nums[middleIndex+2] + ... + nums[nums.length-1].
+If middleIndex == 0, the left side sum is considered to be 0. Similarly, if middleIndex == nums.length - 1, the right side sum is considered to be 0.
+Return the leftmost middleIndex that satisfies the condition, or -1 if there is no such index.
+
+Example 1:
+Input: nums = [2,3,-1,8,4]
+Output: 3
+Explanation: The sum of the numbers before index 3 is: 2 + 3 + -1 = 4
+The sum of the numbers after index 3 is: 4 = 4
+
+Example 2:
+Input: nums = [1,-1,4]
+Output: 2
+Explanation: The sum of the numbers before index 2 is: 1 + -1 = 0
+The sum of the numbers after index 2 is: 0
+
+Example 3:
+Input: nums = [2,5]
+Output: -1
+Explanation: There is no valid middleIndex.
+
+Constraints:
+1 <= nums.length <= 100
+-1000 <= nums[i] <= 1000
+
+Note: This question is the same as 724: https://leetcode.com/problems/find-pivot-index/
+*/
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) 
+    {
+        int leftsum = 0, rightsum = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            leftsum += nums[i];
+        }
+        
+        rightsum = leftsum;
+        leftsum = 0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            rightsum = rightsum - nums[i];
+            
+            cout << leftsum << " " << rightsum << endl;
+            if(leftsum == rightsum)
+            {
+                return i;
+            }
+            
+            leftsum += nums[i];
+        }
+        return -1;
+    }
+};
