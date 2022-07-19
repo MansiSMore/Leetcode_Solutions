5fe8bd4673cbcf5ede4bab022da5a961eb9f55f3
Mansi More <45144625+MansiSMore@users.noreply.github.com>
2022-07-19 21:03:53 +0530

    Create 2341_Maximum_Number_of_Pairs_in_Array.cpp

---
+++ 2341_Maximum_Number_of_Pairs_in_Array.cpp
@@ -0,0 +1,60 @@
+/*
+You are given a 0-indexed integer array nums. In one operation, you may do the following:
+Choose two integers in nums that are equal.
+Remove both integers from nums, forming a pair.
+The operation is done on nums as many times as possible.
+Return a 0-indexed integer array answer of size 2 where answer[0] is the number of pairs that are formed and 
+answer[1] is the number of leftover integers in nums after doing the operation as many times as possible.
+
+Example 1:
+Input: nums = [1,3,2,1,3,2,2]
+Output: [3,1]
+Explanation:
+Form a pair with nums[0] and nums[3] and remove them from nums. Now, nums = [3,2,3,2,2].
+Form a pair with nums[0] and nums[2] and remove them from nums. Now, nums = [2,2,2].
+Form a pair with nums[0] and nums[1] and remove them from nums. Now, nums = [2].
+No more pairs can be formed. A total of 3 pairs have been formed, and there is 1 number leftover in nums.
+
+Example 2:
+Input: nums = [1,1]
+Output: [1,0]
+Explanation: Form a pair with nums[0] and nums[1] and remove them from nums. Now, nums = [].
+No more pairs can be formed. A total of 1 pair has been formed, and there are 0 numbers leftover in nums.
+
+Example 3:
+Input: nums = [0]
+Output: [0,1]
+Explanation: No pairs can be formed, and there is 1 number leftover in nums.
+
+Constraints:
+1 <= nums.length <= 100
+0 <= nums[i] <= 100
+*/
+class Solution {
+public:
+    vector<int> numberOfPairs(vector<int>& nums) 
+    {
+        unordered_map<int, int> uobj;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(uobj[nums[i]] == 0)
+            {
+                uobj[nums[i]] = 1;
+            }
+            else
+            {
+                uobj[nums[i]] += 1;
+            }
+        }
+        vector<int> ans;
+        int cnt1 = 0, cnt2 = 0;
+        for(auto x : uobj)
+        {
+            cnt1 += (x.second / 2);
+            cnt2 += (x.second % 2);           
+        }
+        ans.push_back(cnt1);
+        ans.push_back(cnt2);
+        return ans;
+    }
+};
