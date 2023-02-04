de63b8b52f4120fe1f0d7a8d1ef8c9a181833f72
Mansi More <45144625+MansiSMore@users.noreply.github.com>
2023-02-04 22:19:16 +0530

    Create 2553_Separate_the_Digits_in_an_Array.cpp

---
+++ 2553_Separate_the_Digits_in_an_Array.cpp
@@ -0,0 +1,53 @@
+/*
+Given an array of positive integers nums, 
+return an array answer that consists of the digits of each integer in nums after separating them in the same order they appear in nums.
+To separate the digits of an integer is to get all the digits it has in the same order.
+For example, for the integer 10921, the separation of its digits is [1,0,9,2,1].
+
+Example 1:
+Input: nums = [13,25,83,77]
+Output: [1,3,2,5,8,3,7,7]
+Explanation: 
+- The separation of 13 is [1,3].
+- The separation of 25 is [2,5].
+- The separation of 83 is [8,3].
+- The separation of 77 is [7,7].
+answer = [1,3,2,5,8,3,7,7]. Note that answer contains the separations in the same order.
+
+Example 2:
+Input: nums = [7,1,3,9]
+Output: [7,1,3,9]
+Explanation: The separation of each integer in nums is itself.
+answer = [7,1,3,9].
+
+Constraints:
+1 <= nums.length <= 1000
+1 <= nums[i] <= 105
+*/
+
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) 
+    {
+        vector<int> ans;
+        
+        for(int i = 0; i < nums.size(); i++)
+        {
+            stack<int> sd;
+            while(nums[i] != 0)
+            {
+                int dig = nums[i] % 10;
+                sd.push(dig);
+                nums[i] = nums[i] / 10;
+            }
+            while (!sd.empty())
+            {
+                int digit = sd.top();
+                sd.pop();
+                ans.push_back(digit);
+            }
+
+        }
+        return ans;
+    }
+};
